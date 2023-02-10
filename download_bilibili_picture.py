8bdd91329005c7488f65543953f4cf750b10946c
李世龍 <madaouu@gmail.com>
2023-02-10 20:30:53 +0900

    fix py

--- download_bilibili_picture.py
+++ download_bilibili_picture.py
@@ -14,10 +14,12 @@ def download_image(url, name):
         f.write(response.content)  # 写入图片内容
 
 
-def get_dynamic_images(mids):
+def get_dynamic_images(mid, next_offset=0, limit=20):
     """
     获取某个 up 主的动态图片
     :param mids: up 主的 ID list
+    :next_offset: 偏移量
+    :limit: 每页显示数量
     """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -25,27 +27,35 @@ def get_dynamic_images(mids):
 
     # 构造请求 URL，查询 up 主的动态
     # 发送 GET 请求，获取响应内容，并解析为 JSON 格式
-    for mid in mids:
-        # 构造请求头，模拟浏览器访问
-        url = 'https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid={}'.format(
-            mid)
-        response = requests.get(url, headers=headers).json()
-        items = response.get('data').get('cards')  # 从响应内容中获取动态列表
-        for item in items:  # 遍历动态列表
-            pictures = json.loads(item.get('card')).get('item').get('pictures')  # 获取当前动态中的图片列表
-            if not pictures:  # 如果当前动态没有图片，跳过
+    # 构造请求头，模拟浏览器访问
+    url = f'https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid={mid}&offset_dynamic_id={next_offset}&need_top=0&limit={limit}'
+    response = requests.get(url, headers=headers).json()
+    data = response.get('data')
+    if not data:
+        return
+    items = data.get('cards', [])  # 从响应内容中获取动态列表
+    next_offset = data.get('next_offset')
+    for item in items:  # 遍历动态列表
+        item = json.loads(item.get('card')).get(
+            'item')
+        if not item:
+            continue
+        pictures = item.get('pictures')  # 获取当前动态中的图片列表
+        if not pictures:  # 如果当前动态没有图片，跳过
+            continue
+        for picture in pictures:  # 遍历图片列表
+            image_url = picture.get('img_src')  # 获取图片链接
+            if not image_url:  # 如果图片链接不存在，
                 continue
-            for picture in pictures:  # 遍历图片列表
-                image_url = picture.get('img_src')  # 获取图片链接
-                if not image_url:  # 如果图片链接不存在，
-                    continue
-                filename = os.path.join('images', image_url.split('/')
-                                        [-1])  # 构造图片文件名，以图片链接中的最后一段为名称
-                download_image(image_url, filename)  # 下载图片
+            filename = os.path.join('images', image_url.split('/')
+                                    [-1])  # 构造图片文件名，以图片链接中的最后一段为名称
+            download_image(image_url, filename)  # 下载图片
+            next_offset = next_offset + 1
+            get_dynamic_images(mid, next_offset, limit)
 
 
 if __name__ == '__main__':
     # mid = input('请输入up主的ID：')  # 输入 up 主的 ID
     if not os.path.exists('images'):  # 如果 images 文件夹不存在，创建
         os.makedirs('images')
-    get_dynamic_images(['13127564'])  # 获取 up 主的动态图片
+    get_dynamic_images('13127564')  # 获取 up 主的动态图片
